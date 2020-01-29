9a44e1f29b3e9e6ef2a3b179978c5de6229b0545
Tom Drabas <todrabas@microsoft.com>
2020-01-29 15:29:20 -0800

    Splitting scheduler and worker startup scripts

---
+++ dask_cloudprovider/providers/azureml/setup/start_worker.py
@@ -0,0 +1,58 @@
+# Imports 
+import os
+import sys
+import uuid
+import time
+import socket
+import argparse
+import threading
+import subprocess
+
+from mpi4py import MPI
+from azureml.core import Run
+from notebook.notebookapp import list_running_servers
+
+def flush(proc, proc_log):
+    while True:
+        proc_out = proc.stdout.readline()
+        if proc_out == '' and proc.poll() is not None:
+            proc_log.close()
+            break
+        elif proc_out:
+            sys.stdout.write(proc_out)
+            proc_log.write(proc_out)
+            proc_log.flush()
+
+if __name__ == '__main__':
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+    ### PARSE ARGUMENTS
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--code_store",        default=None)
+    parser.add_argument("--data_store",        default=None)
+    parser.add_argument("--scheduler_ip_port", default=None)
+    parser.add_argument("--use_GPU",           default=False)
+    parser.add_argument("--n_gpus_per_node",   default=0)
+    # parser.add_argument("--script")
+
+    args, unparsed = parser.parse_known_args()
+    
+    ### CONFIGURE GPU RUN
+    GPU_run = args.use_GPU
+
+    if GPU_run:
+        n_gpus_per_node = eval(args.n_gpus_per_node)
+
+    ip = socket.gethostbyname(socket.gethostname())
+    
+    if not GPU_run:
+        cmd = "dask-worker " + scheduler 
+    else:
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(list(range(n_gpus_per_node))).strip("[]")
+        cmd = "dask-cuda-worker " + scheduler + " --memory-limit 0"
+                
+    worker_log = open("worker_{rank}_log.txt".format(rank=rank), "w")
+    worker_proc = subprocess.Popen(cmd.split(), universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    flush(worker_proc, worker_log)
