4bac8579fbb630108d9e630ea2576ca0b17c18fe
Tom Drabas <todrabas@microsoft.com>
2020-01-30 13:47:02 -0800

    Major API refactor started

--- dask_cloudprovider/providers/azureml/azureml.py
+++ dask_cloudprovider/providers/azureml/azureml.py
@@ -9,155 +9,156 @@ class AzureMLCluster:
     def __init__(self
         , workspace                     # AML workspace object
         , compute                       # AML compute object
-        , initial_node_count            # initial node count, must be less than
+        , initial_node_count = 1        # initial node count, must be less than
                                         # or equal to AML compute object max nodes
-        , environment_name              # name of the environment to use
-        , experiment_name               # name of the experiment to start
+                                        # will default to max nodes if more than
+        , experiment_name='DaskAML'     # name of the experiment to start
+        , use_existing_run=False        # use existing run if any
+        , environment_definition=None   # name of the environment to use
+        , pip_packages=None             # list of pip packages to install
+        , conda_packages=None           # list of conda packages to install 
+        , use_GPU=False                 # flag to indicate GPU vs CPU cluster
+        , n_gpus_per_node=None          # number of GPUs per node if use_GPU flag set
         , docker_image=None             # optional -- docker image
-        , jupyter_port=9000
-        , dask_dashboard_port=9001
-        , codefileshare=None
-        , datafileshare=None
-        , update_environment=False
-        , use_GPU=False
-        , gpus_per_node=None
-        , use_existing_run=False
+        , jupyter=True                  # start Jupyter lab process on headnode
+        , jupyter_port=9000             # port to forward the Jupyter process to
+        , dashboard_port=9001           # port to forward Dask dashboard to
+        , datastores=[]
         , **kwargs
     ):
+        ### GENERAL FLAGS
         self.workspace = workspace
         self.compute = compute
         self.initial_node_count = initial_node_count
-        self.environment_name = environment_name
-        self.environment_obj = None
+
+        ### EXPERIMENT DEFINITION
         self.experiment_name = experiment_name
-        self.update_environment = update_environment
-        self.docker_image = docker_image
-        self.jupyter_port = jupyter_port
-        self.dask_dashboard_port = dask_dashboard_port
-        self.codefileshare = codefileshare
-        self.datafileshare = self.workspace.get_default_datastore() if datafileshare == None else datafileshare
-        self.use_GPU = use_GPU
-        self.gpus_per_node = gpus_per_node
         self.use_existing_run = use_existing_run
+
+        ### ENVIRONMENT AND VARIABLES
+        self.environment_definition = environment_definition
+        self.pip_packages = pip_packages 
+        self.conda_packages = conda_packages       
+        
+        ### GPU RUN INFO
+        self.use_GPU = use_GPU
+        self.n_gpus_per_node = n_gpus_per_node
+        
+        ### JUPYTER AND PORT FORWARDING
+        self.jupyter = jupyter
+        self.jupyter_port = jupyter_port
+        self.dashboard_port = dashboard_port
+
+        ### DATASTORES
+        self.datastores = datastores
+        
+        ### FUTURE EXTENSIONS
         self.kwargs = kwargs
-        self.max_nodes = self.compute.serialize()['properties']['properties']['scaleSettings']['maxNodeCount']
-  
+
+        ### PARAMETERS TO START THE CLUSTER
         self.scheduler_params = {}
         self.worker_params = {}
  
         ### CLUSTER PARAMS
+        self.max_nodes = (
+            self
+            .compute
+            .serialize()
+            ['properties']
+            ['properties']
+            ['scaleSettings']
+            ['maxNodeCount']
+        )
         self.scheduler_ip_port = None
         self.workers_list = []
         self.URLs = {}
 
         ### SANITY CHECKS
+        ###-----> initial node count
         if self.initial_node_count > self.max_nodes:
-            raise Exception(f'Initial count of nodes ({self.initial_node_count}) greater than allowed: {self.max_nodes}')
+            self.initial_node_count = self.max_nodes
 
+        ###-----> environment spec
+        if self.environment_definition and (
+               (type(self.pip_packages)   is list and len(self.pip_packages)   > 0)
+            or (type(self.conda_packages) is list and len(self.conda_packages) > 0)
+        ):
+            raise Exception('Cannot specify `environment_definition` and `pip_packages` nor `conda_packages`.')
+
+        ### INITIALIZE CLUSTER
         self.create_cluster()
-
-        # self.__print_message('Initiated')
 
     def __print_message(self, msg, length=80, filler='#', pre_post=''):
         print(f'{pre_post} {msg} {pre_post}'.center(length, filler))
+
+    def __get_estimator(self):
+        return None
 
     def create_cluster(self):
         # set up environment
         self.__print_message('Setting up environment')
 
-        if (
-               self.environment_name not in self.workspace.environments
-            or self.update_environment
-        ):
-            self.__print_message('Rebuilding environment')
-            env = Environment(name=self.environment_name)
-            env.docker.enabled = True
-            env.docker.base_image = self.docker_image
 
-            if self.use_GPU and 'python_interpreter' in self.kwargs:
-                env.python.interpreter_path = self.kwargs['python_interpreter']
-                env.python.user_managed_dependencies = True
+        # 
 
-            ### CHECK IF pip_packages or conda_packages in kwargs
-            conda_dep = None
-            if 'pip_packages' in self.kwargs or 'conda_packages' in self.kwargs:
-                conda_dep = CondaDependencies()
+        # ### scheduler and worker parameters
+        # self.scheduler_params['--jupyter'] = True
+        # self.scheduler_params['--code_store'] = self.workspace.datastores[self.codefileshare]
+        # self.scheduler_params['--data_store'] = self.workspace.datastores[self.datafileshare]
 
-            if 'pip_packages' in self.kwargs:
-                if self.kwargs['pip_packages'] is list:
-                    for pip_package in self.kwargs['pip_packages']:
-                        conda_dep.add_pip_package(pip_package)
+        # self.worker_params['--code_store'] = self.workspace.datastores[self.codefileshare]
+        # self.worker_params['--data_store'] = self.workspace.datastores[self.datafileshare]
 
-            if 'conda_packages' in self.kwargs:
-                if self.kwargs['pip_packages'] is list:
-                    for conda_package in self.kwargs['conda_packages']:
-                        conda_dep.add_conda_package(conda_package)
+        # if self.use_GPU:
+        #     self.scheduler_params['--use_GPU'] = True
+        #     self.scheduler_params['--n_gpus_per_node'] = self.gpus_per_node
+        #     self.worker_params['--use_GPU'] = True
+        #     self.worker_params['--n_gpus_per_node'] = self.gpus_per_node
 
-            if conda_dep is not None:
-                env.python.conda_dependencies = conda_dep
+        # # submit run
+        # self.__print_message('Submitting the experiment')
+        # exp = Experiment(self.workspace, self.experiment_name)
 
-            self.environment_obj = env.register(self.workspace)
-        else:
-            self.environment_obj = self.workspace.environments[self.environment_name]
+        # run = None
+        # if self.use_existing_run == True:
+        #     runs = exp.get_runs()
 
-        ### scheduler and worker parameters
-        self.scheduler_params['--jupyter'] = True
-        self.scheduler_params['--code_store'] = self.workspace.datastores[self.codefileshare]
-        self.scheduler_params['--data_store'] = self.workspace.datastores[self.datafileshare]
-
-        self.worker_params['--code_store'] = self.workspace.datastores[self.codefileshare]
-        self.worker_params['--data_store'] = self.workspace.datastores[self.datafileshare]
-
-        if self.use_GPU:
-            self.scheduler_params['--use_GPU'] = True
-            self.scheduler_params['--n_gpus_per_node'] = self.gpus_per_node
-            self.worker_params['--use_GPU'] = True
-            self.worker_params['--n_gpus_per_node'] = self.gpus_per_node
-
-        # submit run
-        self.__print_message('Submitting the experiment')
-        exp = Experiment(self.workspace, self.experiment_name)
-
-        run = None
-        if self.use_existing_run == True:
-            runs = exp.get_runs()
-
-            try:
-                run = next(x for x in runs if ('scheduler' in x.get_metrics() and x.get_status() == 'Running'))
-            except StopIteration:
-                run = None
-                self.__print_message('NO EXISTING RUN WITH SCHEDULER', filler='!')
+        #     try:
+        #         run = next(x for x in runs if ('scheduler' in x.get_metrics() and x.get_status() == 'Running'))
+        #     except StopIteration:
+        #         run = None
+        #         self.__print_message('NO EXISTING RUN WITH SCHEDULER', filler='!')
         
-        if not run:
-            est = Estimator(
-                  'dask_cloudprovider/providers/azureml/setup'
-                , compute_target=self.compute
-                , entry_script='start_scheduler.py'
-                , environment_definition=self.environment_obj
-                , script_params=self.scheduler_params
-                , node_count=1 ### start only scheduler
-                , distributed_training=MpiConfiguration()
-                , use_docker=True
-            )
+        # if not run:
+        #     est = Estimator(
+        #           'dask_cloudprovider/providers/azureml/setup'
+        #         , compute_target=self.compute
+        #         , entry_script='start_scheduler.py'
+        #         , environment_definition=self.environment_obj
+        #         , script_params=self.scheduler_params
+        #         , node_count=1 ### start only scheduler
+        #         , distributed_training=MpiConfiguration()
+        #         , use_docker=True
+        #     )
 
-            run = exp.submit(est)
+        #     run = exp.submit(est)
 
-            self.__print_message("Waiting for scheduler node's ip")
-            while (
-                run.get_status() != 'Canceled' 
-                and 'scheduler' not in run.get_metrics()
-            ):
-                print('.', end="")
-                time.sleep(5)
+        #     self.__print_message("Waiting for scheduler node's ip")
+        #     while (
+        #         run.get_status() != 'Canceled' 
+        #         and 'scheduler' not in run.get_metrics()
+        #     ):
+        #         print('.', end="")
+        #         time.sleep(5)
             
-            print('\n\n')
+        #     print('\n\n')
             
-            self.scheduler_ip_port = run.get_metrics()["scheduler"]
-            self.worker_params['--scheduler_ip_port'] = self.scheduler_ip_port
-            self.__print_message(f'Scheduler: {run.get_metrics()["scheduler"]}')
+        #     self.scheduler_ip_port = run.get_metrics()["scheduler"]
+        #     self.worker_params['--scheduler_ip_port'] = self.scheduler_ip_port
+        #     self.__print_message(f'Scheduler: {run.get_metrics()["scheduler"]}')
 
-        self.run = run
-        self.scale(self.initial_node_count - 1)
+        # self.run = run
+        # self.scale(self.initial_node_count - 1)
 
     def connect_cluster(self):
         if not self.run:
