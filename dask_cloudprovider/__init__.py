425001813600d9ee040a53d28a0e68f9791664bc
Fred Li <feli1@microsoft.com>
2020-04-25 00:22:43 -0700

    Update init

--- dask_cloudprovider/__init__.py
+++ dask_cloudprovider/__init__.py
@@ -1,8 +1,8 @@
 from . import config
 from .providers.aws.ecs import ECSCluster, FargateCluster
-from .providers.azure.azureml import AzureMLCluster
+from .providers.azure.azureml import AzureMLCluster, AzureMLSSHCluster
 
-__all__ = ["ECSCluster", "FargateCluster", "AzureMLCluster"]
+__all__ = ["ECSCluster", "FargateCluster", "AzureMLCluster", "AzureMLSSHCluster"]
 
 from ._version import get_versions
 
