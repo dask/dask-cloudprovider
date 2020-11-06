be6802b1654d053e0d328af186a91e7f4645d871
Jacob Tomlinson <jtomlinson@nvidia.com>
2020-11-06 11:20:04 +0000

    Remove fixture

--- dask_cloudprovider/generic/tests/test_vmcluster.py
+++ dask_cloudprovider/generic/tests/test_vmcluster.py
@@ -4,6 +4,6 @@ from dask_cloudprovider.generic.vmcluster import VMCluster
 
 
 @pytest.mark.asyncio
-async def test_init(gen_cluster):
+async def test_init():
     with pytest.raises(RuntimeError):
         _ = VMCluster(asynchronous=True)
