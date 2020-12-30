4ef94430274b208b435b0e81a4fa84b476600a34
Rafal Wojdyla <ravwojdyla@gmail.com>
2020-12-30 12:19:22 +0100

    Add util to check if running in GCE env

--- dask_cloudprovider/gcp/utils.py
+++ dask_cloudprovider/gcp/utils.py
@@ -12,3 +12,22 @@ def build_request(credentials=None):
         return googleapiclient.http.HttpRequest(new_http, *args, **kwargs)
 
     return inner
+
+
+def is_inside_gce() -> bool:
+    """
+    Returns True is the client is running in the GCE environment,
+    False otherwise.
+
+    Doc: https://cloud.google.com/compute/docs/storing-retrieving-metadata
+    """
+    h = httplib2.Http()
+    try:
+        resp_headers, _ = h.request(
+            "http://metadata.google.internal/computeMetadata/v1/",
+            headers={"metadata-flavor": "Google"},
+            method="GET",
+        )
+    except (httplib2.HttpLib2Error, OSError):
+        return False
+    return True
