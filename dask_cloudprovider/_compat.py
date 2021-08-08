61a510ff1e2ece6f84f3acbb53fc23efe352a2b5
James Lamb <jaylamb20@gmail.com>
2021-08-08 18:52:39 -0500

    linting

--- dask_cloudprovider/_compat.py
+++ dask_cloudprovider/_compat.py
@@ -6,6 +6,6 @@ compatible with a wide range of versions of its dependencies.
 # distributed.utils.serialize_for_cli was moved to dask
 # as of https://github.com/dask/distributed/pull/4966
 try:
-    from dask.config import serialize as dask_serialize
+    from dask.config import serialize as dask_serialize  # noqa: F401
 except ImportError:
-    from distributed.utils import serialize_for_cli as dask_serialize
+    from distributed.utils import serialize_for_cli as dask_serialize  # noqa: F401
