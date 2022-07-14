8394015bb30efe5e9eed430bf5dee7ff06fd21ef
jkanche <jayaram.kancherla@gmail.com>
2022-07-14 14:51:58 -0700

    feat: append uuid to the default worker definition

--- dask_cloudprovider/aws/ecs.py
+++ dask_cloudprovider/aws/ecs.py
@@ -766,7 +766,7 @@ class ECSCluster(SpecCluster):
         self._worker_mem = worker_mem
         self._worker_gpu = worker_gpu
         self._worker_extra_args = worker_extra_args
-        self._worker_task_def_name = worker_task_def_name if worker_task_def_name else "dask-worker"
+        self._worker_task_def_name = worker_task_def_name if worker_task_def_name else "dask-worker-{}".format(str(uuid.uuid4())[:5])
         self._worker_task_kwargs = worker_task_kwargs
         self._n_workers = n_workers
         self._workers_name_start = workers_name_start
