a7148d82749fabcc0cb44041078818db409f3539
Daniel Alejandro Coll Tejeda <62675074+macarronesc@users.noreply.github.com>
2024-06-13 11:14:45 +0000

    Working on IBM Code Engine support

--- dask_cloudprovider/ibm/code_engine.py
+++ dask_cloudprovider/ibm/code_engine.py
@@ -1,19 +1,18 @@
 import dask
 from dask_cloudprovider.config import ClusterConfig
+import dask.config
+
 from dask_cloudprovider.generic.vmcluster import (
     VMCluster,
     VMInterface,
     SchedulerMixin,
     WorkerMixin,
 )
-from typing import Optional
-import os
-
+import time
 
 try:
     from ibm_code_engine_sdk.code_engine_v2 import CodeEngineV2
     from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-    from ibm_code_engine_sdk.code_engine_v2 import ProjectsPager
 except ImportError as e:
     msg = (
         "Dask Cloud Provider IBM requirements are not installed.\n\n"
@@ -22,8 +21,6 @@ except ImportError as e:
         '  pip install "dask-cloudprovider[ibm]" --upgrade       # or python -m pip install'
     )
     raise ImportError(msg) from e
-import json
-
 
 
 class IBMCodeEngine(VMInterface):
@@ -41,34 +38,51 @@ class IBMCodeEngine(VMInterface):
         super().__init__(*args, **kwargs)
         self.cluster = cluster
         self.config = config
-
+        self.image = image
         self.region = region
         self.project_id = project_id
         self.api_key = api_key
 
-
-    async def create_vm(self):
         authenticator = IAMAuthenticator(self.api_key, url='https://iam.cloud.ibm.com')
         authenticator.set_disable_ssl_verification(True)  # Disable SSL verification for the authenticator
-        code_engine_service = CodeEngineV2(authenticator=authenticator)
-        code_engine_service.set_service_url('https://api.'+self.region+'.codeengine.cloud.ibm.com/v2')
-        code_engine_service.set_disable_ssl_verification(True)  # Disable SSL verification for the service instance
 
-        """response = code_engine_service.create_job(
-            project_id='',
-            image_reference='icr.io/codeengine/helloworld',
-            name='my-job',
+        self.code_engine_service = CodeEngineV2(authenticator=authenticator)
+        self.code_engine_service.set_service_url('https://api.' + self.region + '.codeengine.cloud.ibm.com/v2')
+        self.code_engine_service.set_disable_ssl_verification(True)  # Disable SSL verification for the service instance
+
+    async def create_vm(self):
+
+        response = self.code_engine_service.create_app(
+            project_id=self.project_id,
+            image_reference=self.image,
+            name='my-app',
+            run_commands=['python', '-m', 'distributed.cli.dask_scheduler'],
+            image_port=8786,
         )
-        job = response.get_result()
-        print(job)"""
+        app = response.get_result()
 
+        while True:
+            response = self.code_engine_service.get_app(
+                project_id=self.project_id,
+                name='my-app',
+            )
+            app = response.get_result()
+            if app["status"] == "ready":
+                break
+            
+            time.sleep(1)
 
-        return private_ip_address, None
+        internal_url = app["endpoint_internal"].split("//")[1]
+        external_url = app["endpoint"].split("//")[1]
 
-
+        return internal_url, external_url
 
     async def destroy_vm(self):
-        pass
+        response = self.code_engine_service.delete_app(
+            project_id=self.project_id,
+            name='my-app',
+        )
+
 
 class IBMCodeEngineScheduler(SchedulerMixin, IBMCodeEngine):
     pass
@@ -79,7 +93,7 @@ class IBMCodeEngineWorker(WorkerMixin, IBMCodeEngine):
 
 
 class IBMCodeEngineCluster(VMCluster):
-     def __init__(
+    def __init__(
         self,
         image: str = None,
         region: str = None,
@@ -91,16 +105,17 @@ class IBMCodeEngineCluster(VMCluster):
         self.scheduler_class = IBMCodeEngineScheduler
         self.worker_class = IBMCodeEngineWorker
         
+        self.image = image or self.config.get("image")
         self.region = region or self.config.get("region")
         self.project_id = project_id or self.config.get("project_id")
-
-        api_key=self.config.get("api_key")
+        api_key = self.config.get("api_key")
 
         self.debug = debug
         
         self.options = {
             "cluster": self,
             "config": self.config,
+            "image": self.image,
             "region": self.region,
             "project_id": self.project_id,
             "api_key": api_key,
