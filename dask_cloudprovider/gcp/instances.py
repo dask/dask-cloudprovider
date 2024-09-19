1d4546bac5a682889618dc593b6e23e1db77df95
Dmitry Balabka <dmitry.balabka@ecentria.com>
2024-09-19 18:45:20 +0300

    Fix tests

--- dask_cloudprovider/gcp/instances.py
+++ dask_cloudprovider/gcp/instances.py
@@ -19,7 +19,7 @@ from distributed.core import Status
 
 try:
     import googleapiclient.discovery
-    import google.auth.default
+    from google.auth import default as google_auth_default
     from googleapiclient.errors import HttpError
 except ImportError as e:
     msg = (
@@ -672,7 +672,7 @@ class GCPCompute:
             )
         else:
             # Obtain Application Default Credentials
-            credentials, _ = google.auth.default()
+            credentials, _ = google_auth_default()
 
         # Use the credentials to build a service client
         return googleapiclient.discovery.build(
